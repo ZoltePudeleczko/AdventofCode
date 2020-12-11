17d2c1bec3889fd5ed8a60de74c77579a6fe9f6e
Szymon Zborowski <szzborowski@gmail.com>
2020-12-11 08:37:52 +0100

    Add run.bat, move source files to src

---
+++ run.bat
@@ -0,0 +1,11 @@
+python src/Day1.py
+python src/Day2.py
+python src/Day3.py
+python src/Day4.py
+python src/Day5.py
+python src/Day6.py
+python src/Day7.py
+python src/Day8.py
+python src/Day9.py
+python src/Day10.py
+pause
