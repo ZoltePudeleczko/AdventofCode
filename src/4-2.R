336d2af88f16de8bfa3c27f51272bd9415803538
Szymon Zborowski <szymon.zborowski@leancode.pl>
2022-12-05 17:30:05 +0100

    Finish day4

---
+++ src/4-2.R
@@ -0,0 +1,17 @@
+library(purrr)
+
+as_sequences <- function(x) {
+    list(seq(x[1], x[2]), seq(x[3], x[4]))
+}
+
+overlaps <- function(x) {
+    length(intersect(unlist(x[1]), unlist(x[2]))) > 0
+}
+
+input <- readLines("inputs/day4.txt") |>
+    strsplit("[-,]") |>
+    map(as.integer) %>%
+    map(as_sequences) %>%
+    map_lgl(overlaps) %>%
+    sum() %>%
+    print()
